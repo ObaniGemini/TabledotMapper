b008feedf644f8bca8c793e49c981770a97dcc40
ObaniGemini <robin@pi-et-ro.net>
2025-12-29 19:50:48 +0100

    fullscreen key

--- main.gd
+++ main.gd
@@ -80,3 +80,8 @@ func get_pattern_offset(): return $UI.patternparameters.pattern_offset()
 func get_pattern_brush_size(): return $UI.patternparameters.brush_size()
 func get_pattern_brush_roughness(): return $UI.patternparameters.brush_roughness()
 func get_pattern_brush_color(): return $UI.patternparameters.brush_color()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		get_window().mode = Window.MODE_WINDOWED if get_window().mode == Window.MODE_FULLSCREEN else Window.MODE_FULLSCREEN
