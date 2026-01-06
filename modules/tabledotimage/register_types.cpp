336a50dd9d5e84048821961a105d5699ca768923
ObaniGemini <robin@pi-et-ro.net>
2026-01-06 19:26:04 +0100

    Add boilerplate for GDExtension (not tested yet)

--- modules/tabledotimage/register_types.cpp
+++ modules/tabledotimage/register_types.cpp
@@ -2,19 +2,34 @@
 
 #include "register_types.h"
 
-#include "core/object/class_db.h"
-#include "tabledotimage.h"
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
 
 void initialize_tabledotimage_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	ClassDB::register_class<TabledotImage>();
+	GDREGISTER_RUNTIME_CLASS(TabledotImage);
 }
 
 void uninitialize_tabledotimage_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-   // Nothing to do here in this example.
+}
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT tabledotimage_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_tabledotimage_module);
+	init_obj.register_terminator(uninitialize_tabledotimage_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
 }
--- modules/tabledotimage/register_types.h
+++ modules/tabledotimage/register_types.h
@@ -1,7 +1,8 @@
 /* register_types.h */
 
-#include "modules/register_module_types.h"
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
 
 void initialize_tabledotimage_module(ModuleInitializationLevel p_level);
 void uninitialize_tabledotimage_module(ModuleInitializationLevel p_level);
-/* yes, the word in the middle must be the same as the module folder name */
